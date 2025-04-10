1e3ea07b98c4b533a642a9604f8d41fb99a7dd2d
Sugam Gelal <sugamgelal@gmail.com>
2025-04-10 16:51:03 +0545

    ssds

--- apps/front/vite.config.js
+++ apps/front/vite.config.js
@@ -1,5 +1,5 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig, loadEnv } from "vite";
