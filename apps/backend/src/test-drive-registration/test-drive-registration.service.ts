e20b70495fed34e8a7af13fcf3ce59ae024c5300
Sugam Gelal <sugamgelal@gmail.com>
2025-04-12 10:49:58 +0545

    fix: send SMS notification for approved test drive registrations and update baseURL configuration

--- apps/backend/src/test-drive-registration/test-drive-registration.service.ts
+++ apps/backend/src/test-drive-registration/test-drive-registration.service.ts
@@ -111,6 +111,14 @@ export class TestDriveRegistrationService {
     updateTestDriveRegistrationDto.status
       ? (testDriveRegistration.status = updateTestDriveRegistrationDto.status)
       : (testDriveRegistration.status = testDriveRegistration.status);
+
+    if (updateTestDriveRegistrationDto.status === 'approved') {
+      console.log(updateTestDriveRegistrationDto.date);
+      sendSms(
+        testDriveRegistration.phone,
+        `Dear ${testDriveRegistration.firstName}, Your Test Drive on Drive Skywell 2025 NepalToEurope is scheduled on ${new Date(updateTestDriveRegistrationDto.date).toISOString().split('T')[0]} . We will contact you for further process!-Skywell Nepal`,
+      );
+    }
     return runInTransaction((queryRunner) =>
       queryRunner.manager.save(TestDriveRegistration, testDriveRegistration),
     );
